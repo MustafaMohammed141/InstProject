6a53fc8f1e1ef5b33921e799f73a6171ecbec4eb
Mustafa <68327651+MustafaMohammed141@users.noreply.github.com>
2025-05-05 01:40:04 +0300

    Update Home.java

--- src/Home.java
+++ src/Home.java
@@ -1,5 +1,4 @@
 
-import com.formdev.flatlaf.FlatDarkLaf;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -111,7 +110,6 @@ public class Home extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        FlatDarkLaf.setup();
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
